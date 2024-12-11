12e26f38bff560818fb8db8a4a01ccc8d760013d
Ahmad Zidni Hidayat <zdngaming312@gmail.com>
2024-12-11 22:09:01 +0700

    update ui risk

---
+++ resources/js/Constants/LikelihoodColorMapping.ts
@@ -0,0 +1,22 @@
+export const LikelihoodColorMapping = {
+    1: {
+        label: "Very Low",
+        color: "bg-blue-200 text-blue-800",
+    },
+    2: {
+        label: "Low",
+        color: "bg-green-200 text-green-800",
+    },
+    3: {
+        label: "Moderate",
+        color: "bg-yellow-200 text-yellow-800",
+    },
+    4: {
+        label: "High",
+        color: "bg-orange-200 text-orange-800",
+    },
+    5: {
+        label: "Very High",
+        color: "bg-red-200 text-red-800",
+    },
+} as const;
